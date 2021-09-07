9b69a68c1aaad42c4de6f58bcbaa7ca7184e5ff0
lucas3d4783 <lucas3d4783@gmail.com>
2021-09-07 17:44:33 -0300

    Adicionando Seeder de User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,15 +6,18 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        User::factory(1)->create([
+            'email' => 'lucas@teste.com',
+            'password' => Hash::make('password')
+        ]);
     }
 }
